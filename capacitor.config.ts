4e8febb9aa67c8883918854a17f7eea87b9bbd0b
jhonforero44 <jhonforero44@gmail.com>
2025-03-30 22:55:19 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyecto-MercaBit-Git',
+  webDir: 'dist'
+};
+
+export default config;
