71087f47ff499f86fbb9cf4f409855ad6ce6e970
JhonForero44 <jhonforero44@gmail.com>
2025-04-14 15:14:41 -0500

    Configuracion encabezado

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,5 +1,13 @@
 <template>
   <ion-app>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button />
+        </ion-buttons>
+        <ion-title>{{ pageTitle }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
     <!-- Menú lateral -->
     <MenuDiagonal />
     <ion-router-outlet id="main-content"></ion-router-outlet>
@@ -16,7 +24,26 @@ import { IonApp, IonMenu, IonPage, IonRouterOutlet } from '@ionic/vue';
 import MenuDiagonal from '@/components/MenuDiagonal.vue'
 import { useRoute } from 'vue-router';
 import Footer from '@/components/Footer.vue'
+import { computed } from 'vue'
 
 const route = useRoute();
+
+// Mapeo de rutas a títulos
+const pageTitle = computed(() => {
+  const titles = {
+    '/login': 'Inicio de Sesion',
+    '/registro': 'Registro',
+    '/recuperar-contrasena': 'Recuperar Contraseña',
+    '/accerca-de-la-app': 'Acerca de MercaBit',
+    '/terminos-condiciones': 'Términos y Condiciones',
+    '/notification': 'Notificaciones',
+    '/mis-publicaciones': 'Mis Publicaciones',
+    '/agregar-producto': 'Agregar Producto',
+    '/categorias': 'Categorías',
+    '/informacion-personal': 'Informacion Personal',
+    '/mis-compras': 'Mis Compras',
+  }
+  return titles[route.path] || ''
+})
 
 </script>
