ca0c8c0b018c19ed75feefa02493ad689ba48545
Jhon Forero <jhonforero44@gmail.com>
2025-05-03 01:36:54 -0500

    Modificacion de servicios obtenerCategorias

--- src/services/categoriaService.js
+++ src/services/categoriaService.js
@@ -1,10 +1,22 @@
 import { db } from '../firebase/FirebaseConfig'; // Importa tu configuración de Firebase
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, getDoc, doc } from 'firebase/firestore'
 
 export async function obtenerCategorias() {
-    const categoriasSnapshot = await getDocs(collection(db, 'categorias'))
-    return categoriasSnapshot.docs.map(doc => ({
-      id: doc.id,
-      ...doc.data()
-    }))
+  const categoriasSnapshot = await getDocs(collection(db, 'categorias'))
+  return categoriasSnapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  }))
+}
+
+export async function obtenerCategoriaPorId(categoriaId) {
+  console.log('Categoria ID:', categoriaId);  // Verifica el valor de categoriaId
+  const categoriaRef = doc(db, 'categorias', categoriaId);
+  const categoriaSnapshot = await getDoc(categoriaRef);
+  
+  if (categoriaSnapshot.exists()) {
+    return categoriaSnapshot.data();
+  } else {
+    throw new Error('Categoría no encontrada');
   }
+}
