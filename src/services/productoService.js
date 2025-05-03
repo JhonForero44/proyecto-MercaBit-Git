c8dc152a5f3a048ecf904efaab7d9d8e69f71dc8
Jhon Forero <jhonforero44@gmail.com>
2025-05-03 01:37:42 -0500

    Integracion funcion obtenerProductosPorCategoria

--- src/services/productoService.js
+++ src/services/productoService.js
@@ -1,5 +1,6 @@
 import { db } from '../firebase/FirebaseConfig'; // Importa tu configuración de Firebase
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc, collection, query, where, getDocs } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
 
 // Función para actualizar el precio de un producto
 export const actualizarProducto = async (productoId, nuevoPrecio) => {
@@ -13,3 +14,26 @@ export const actualizarProducto = async (productoId, nuevoPrecio) => {
     console.error('Error al actualizar el producto: ', error);
   }
 };
+
+export async function obtenerProductosPorCategoria(categoriaNombre) {
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  // Asegurarse de que el usuario esté autenticado
+  if (!user) {
+    throw new Error('Usuario no autenticado');
+  }
+
+  try {
+    const q = query(
+      collection(db, 'products'),
+      where('categoria', '==', categoriaNombre)
+    );
+    const querySnapshot = await getDocs(q);
+    const productos = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    return productos;
+  } catch (error) {
+    console.error('Error al obtener productos por categoría:', error);
+    throw error;
+  }
+}
