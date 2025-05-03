41f485527e6cdbc08834271072b228a6fbb1fb2e
Jhon Forero <jhonforero44@gmail.com>
2025-05-03 01:38:17 -0500

    Creacion de vista Productos por Categoria

---
+++ src/views/ProductosPorCategoria.vue
@@ -0,0 +1,58 @@
+<template>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Productos de Categoría</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+  
+      <IonContent>
+        <!-- Loading Spinner -->
+        <IonLoading :isOpen="isLoading" message="Cargando productos..." spinner="crescent" />
+  
+        <div v-if="!isLoading && productos.length === 0">
+          No hay productos para esta categoría.
+        </div>
+  
+        <div v-else class="productos-container">
+          <TarjetaProducto v-for="producto in productos" :key="producto.id" :producto="producto" />
+        </div>
+      </IonContent>
+    </IonPage>
+  </template>
+  
+  <script setup>
+  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonLoading } from '@ionic/vue'
+  import { ref, onMounted } from 'vue'
+  import { useRoute } from 'vue-router'
+  import TarjetaProducto from '@/components/TarjetaProducto.vue'
+  import { obtenerProductosPorCategoria } from '@/services/productoService'
+  import { obtenerCategoriaPorId } from '@/services/categoriaService'
+  
+  const categoria = ref(null)
+  const productos = ref([])
+  const isLoading = ref(true)
+  const route = useRoute()
+  
+  onMounted(async () => {
+    const categoriaId = route.params.categoriaId
+    try {
+      categoria.value = await obtenerCategoriaPorId(categoriaId)
+      productos.value = await obtenerProductosPorCategoria(categoria.value.nombre_categoria)
+    } catch (error) {
+      console.error('Error al obtener los productos de esta categoría:', error)
+    } finally {
+      isLoading.value = false
+    }
+  })
+  </script>
+  
+  <style scoped>
+  .productos-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+    gap: 1rem;
+    padding: 1rem;
+  }
+  </style>
+  
